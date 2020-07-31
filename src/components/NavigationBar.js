3a050164eee45b154ed237b03674e19a2c4d0faf
abarrios1 <abarrios01@aurora.edu>
2020-07-30 22:03:00 -0700

    Testing router and adding page routes.

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -4,6 +4,8 @@ import {Navbar, Nav} from 'react-bootstrap';
 
 import LoginButton from './LoginButton'
 
+import logo from '../images/HomonitorLogo.png'
+
 export class Navigation extends Component {
   render() {
     return (
@@ -13,22 +15,26 @@ export class Navigation extends Component {
           <Nav>
 
             <NavLink className="rounded d-inline p-2 bg-primary text-dark" to="/">
+              <img src={logo} width={30} height={25} alt="Homonitor Logo"/>
+            </NavLink>
+
+            <NavLink className="rounded d-inline p-2 bg-primary text-dark" to="/">
               Home
             </NavLink>
 
-            <NavLink className="rounded d-inline p-2 bg-primary text-dark" to="#">
+            <NavLink className="rounded d-inline p-2 bg-primary text-dark" to="/netstats">
               Network Stats
             </NavLink>
 
-            <NavLink className="rounded d-inline p-2 bg-primary text-dark" to="#">
+            <NavLink className="rounded d-inline p-2 bg-primary text-dark" to="/about">
               About
             </NavLink>
 
           </Nav>
         </Navbar.Collapse>
-        
+
         <LoginButton />
-        
+
       </Navbar>
     )
   }
